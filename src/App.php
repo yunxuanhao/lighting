67141c62ebb642cb43420941dd32107db8099c0a
yunxuan <yunxuan@didichuxing.com>
2019-06-08 00:47:34 +0800

    增加run

--- src/App.php
+++ src/App.php
@@ -16,6 +16,8 @@ class App
 
     private $_env;
 
+    private $_module = '';
+
     protected function __construct()
     {
 
@@ -29,13 +31,27 @@ class App
         return $this->_requestTime;
     }
 
+    public function setModule($module = '')
+    {
+        $this->_module = $module;
+        return $this;
+    }
+
     public function run()
     {
+        // step1 获取当前的module，找到对应的Classes目录
 
+        // step2 分析url，复制到Request，得到controller和action
 
+        // step3 校验controller和action是否存在，是否可执行（校验文件是否存在和命名空间是否正确）
 
+        // step4 exec controller->action （before，main，action）
+
+        // step5 over
 
 
     }
+
+
 
 }
